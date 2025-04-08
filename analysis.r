ea076dad8fc2b961b1c2d26022fa55007999492a
Liu-Fayi <85021501+Liu-Fayi@users.noreply.github.com>
2025-04-07 21:45:46 -0400

    analysis

---
+++ analysis.r
@@ -0,0 +1,37 @@
+normal_time <- c(193, 181, 184, 184, 228, 181, 209, 192, 216, 220, 364, 301, 206)
+
+normal_error <- c(5, 8, 4, 8, 8, 1, 4, 4, 4, 4, 5, 6, 3)
+
+distracted_time <- c(290, 360, 182, 212, 222, 244, 230, 202, 208, 282, 331, 225, 263)
+
+distracted_error <- c(8, 5, 4, 6, 5, 4, 10, 5, 2, 3, 7, 6, 7)
+
+normal_accuracy <- 1 - normal_error / 20
+distracted_accuracy <- 1 - distracted_error / 20
+
+mean_normal_time <- mean(normal_time)
+mean_normal_error <- mean(normal_error)
+mean_distracted_time <- mean(distracted_time)
+mean_distracted_error <- mean(distracted_error)
+mean_normal_accuracy <- mean(normal_accuracy)
+mean_distracted_accuracy <- mean(distracted_accuracy)
+
+sd_normal_time <- sd(normal_time)
+sd_normal_error <- sd(normal_error)
+sd_distracted_time <- sd(distracted_time)
+sd_distracted_error <- sd(distracted_error)
+sd_normal_accuracy <- sd(normal_accuracy)
+sd_distracted_accuracy <- sd(distracted_accuracy)
+
+curve(dnorm(x, mean = mean_normal_time, sd = sd_normal_time), from = 0, to = 500)
+curve(dnorm(x, mean = mean_distracted_time, sd = sd_distracted_time), from = 0, to = 500)
+curve(dnorm(x, mean = mean_normal_error, sd = sd_normal_error), from = 0, to = 20)
+curve(dnorm(x, mean = mean_distracted_error, sd = sd_distracted_error), from = 0, to = 20)
+curve(dnorm(x, mean = mean_normal_accuracy, sd = sd_normal_accuracy))
+curve(dnorm(x, mean = mean_distracted_accuracy, sd = sd_distracted_accuracy))
+
+# t-test for time
+t_test_time <- t.test(normal_time, distracted_time, mu = 0, var.equal = FALSE, alternative = "less", conf.level = 0.95)
+t_test_time_p_value <- t_test_time$p.value
+t_test_accuracy <- t.test(normal_accuracy, distracted_accuracy, mu = 0, var.equal = FALSE, alternative = "greater", conf.level = 0.95)
+t_test_accuracy_p_value <- t_test_accuracy$p.value
